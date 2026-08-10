153ad69e52c7db6c4f215ae072dc673ef1d183d4
Alkanoble <alkanobleparathinall@gmail.com>
2026-08-10 21:40:59 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- VoxCoach-github/Frontend/src/firebase/firebase.js
+++ VoxCoach-github/Frontend/src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCsvtRXB1P6Ed75uqpueG4MsSLerWL-I4",
-  authDomain: "voxcoach-e7f00.firebaseapp.com",
-  projectId: "voxcoach-e7f00",
-  storageBucket: "voxcoach-e7f00.firebasestorage.app",
-  messagingSenderId: "918593924398",
-  appId: "1:918593924398:web:7407861e1e541d986f5ebf"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
